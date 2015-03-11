e21f94e6efb551941c2516c6027ccdf0fe406237
Janos Papp <janos.papp@nettrend.hu>
2015-03-11 13:27:53 +0100

    It calculates the four basic operation correctly

--- spec/CalculatorSpec.php
+++ spec/CalculatorSpec.php
@@ -18,4 +18,44 @@ class CalculatorSpec extends ObjectBehavior
     	$this->getSum()->shouldBe(0);
     }
 
+    /** @test */
+    function it_takes_a_number_as_a_starting_value()
+    {
+        $this->take(5);
+
+        $this->getSum()->shouldBe(5);
+    }    
+
+    /** @test */
+    function it_calculates_the_multiplication()
+    {
+        $this->take(5)->multiply(6);
+
+        $this->getSum()->shouldBe(30);
+    }
+
+    /** @test */
+    function it_calculates_the_division()
+    {
+        $this->take(20)->divide(5);
+
+        $this->getSum()->shouldBe(4);
+    }
+
+    /** @test */
+    function it_calculates_the_addition()
+    {
+        $this->take(5)->add(6);
+
+        $this->getSum()->shouldBe(11);
+    }
+
+    /** @test */
+    function it_calculates_the_substraction()
+    {
+        $this->take(39)->substract(17);
+
+        $this->getSum()->shouldBe(22);
+    }    
+
 }
--- src/Calculator.php
+++ src/Calculator.php
@@ -13,4 +13,66 @@ class Calculator
 	{
 		return $this->sum;
 	}
+
+    /**
+     * It takes the first argument as a starting value.
+     * 
+     * @param  int $num
+     * @return Smartbit\Calculator
+     */
+    public function take($num)
+    {
+        $this->sum += $num;
+
+        return $this;
+    }    
+
+    /**
+     * It multiplies the current value.
+     * 
+     * @param  int $num
+     * @return Smartbit\Calculator
+     */
+    public function multiply($num)
+    {
+        $this->sum *= $num;
+
+        return $this;
+    }
+
+    /**
+     * It divides the current value.
+     * 
+     * @param  int $num
+     * @return Smartbit\Calculator
+     */
+    public function divide($num)
+    {
+        $this->sum /= $num;
+
+        return $this;
+    }
+
+    /**
+     * It's a mask for the take method.
+     * 
+     * @param int $num
+     */
+    public function add($num)
+    {
+        $this->take($num);
+    }
+
+    /**
+     * It substract the given number from the current value.
+     * 
+     * @param  int $num
+     * @return Smartbit\Calculator
+     */
+    public function substract($num)
+    {
+        $this->sum -= $num;
+
+        return $this;
+    }    
 }
